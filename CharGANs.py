8705f2fce6d71b79cdd6d7b382af292e03168c95
Ahmed Sheha <ahmed.sheha097@gmail.com>
2020-09-08 06:38:23 +0200

    Machine’s Personalized Handwritings

---
+++ CharGANs.py
@@ -0,0 +1,252 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import math
+from tensorflow.keras.layers import Dense,Reshape,Flatten,Dropout,LeakyReLU,BatchNormalization,Conv2D,Conv2DTranspose
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.models import model_from_json
+import cv2
+from PIL import Image
+import os
+import  random
+
+
+letters = {"A":0,"B":1,"C":2,"D":3,"E":4,"F":5,"G":6,"H":7,"I":8,"J":9,"K":10,"L":11,"M":12,"N":13,"O":14,"P":
+           15,"Q":16,"R":17,"S":18,"T":19,"U":20,"V":21,"W":22,"X":23,"Y":24,"Z":25," ":26 , ",":27 , ".":28 }
+
+def Model(letter,num_of_epochs,BatchSize):
+    data = []
+    images = os.listdir("Dataset/Chars/"+letter+"/")
+    print(images)
+    for i in images:
+        print(i)
+        image=cv2.imread("Dataset/Chars/"+letter+"/"+i)
+
+        image_from_array = Image.fromarray(image)
+        image_from_array = image_from_array.convert('L')
+        size_image = image_from_array.resize((28,28))
+        data.append(np.array(size_image))
+    print(len(data))
+    print('***********************')
+    plt.imshow(data[0])
+    plt.show()
+    plt.imshow(data[1])
+    plt.show()
+    mydata = np.asarray(data)
+    mydata = mydata / 255
+    mydata = mydata.reshape(-1,28,28,1) * 2. -1.
+    print(mydata.shape)
+    discriminator = Sequential()
+
+    discriminator.add(Conv2D(64 , kernel_size = (5,5) , strides =(2,2) , padding = "same" ,activation =LeakyReLU(0.3),
+                             input_shape =[28,28,1]))
+
+    discriminator.add(Dropout(0.5))
+
+    discriminator.add(Conv2D(128 , kernel_size = (5,5) , strides =(2,2) , padding = "same" ,activation =LeakyReLU(0.3)
+                            ))
+
+    discriminator.add(Dropout(0.5))
+
+    discriminator.add(Flatten())
+
+    discriminator.add(Dense(1,activation='sigmoid'))
+
+
+    input_to_generator = 100
+    generator = Sequential()
+
+    generator.add(Dense(7 * 7 * 128,  input_shape=[input_to_generator]))
+
+    generator.add(Reshape([7,7,128]))
+
+    generator.add(BatchNormalization())
+
+    generator.add(Conv2DTranspose(64,kernel_size=(5,5) , strides=(2,2) , padding ="same" , activation = "relu"))
+
+    generator.add(BatchNormalization())
+
+    generator.add(Conv2DTranspose(1,kernel_size=(5,5) , strides=(2,2) , padding ="same" , activation = "tanh"))
+
+    GAN = Sequential([generator,discriminator])
+    discriminator.compile(loss='binary_crossentropy' , optimizer='adam')
+    discriminator.trainable = False
+    GAN.compile(loss='binary_crossentropy',optimizer='adam')
+
+    batch_size = BatchSize
+    my_data = mydata
+    dataset=tf.data.Dataset.from_tensor_slices(my_data).shuffle(buffer_size = 1000)
+
+    dataset = dataset.batch(batch_size, drop_remainder = True).prefetch(1)
+    epochs = num_of_epochs
+    generator , discriminator = GAN.layers
+
+    for epoch in range(epochs):
+        print(f"Epoch #{epoch+1}")
+        i = 0
+        for x_batch in dataset:
+            i = i +1
+            if i%50 == 0 :
+                print(f'\t Batch #{i}')
+            noisy_image = tf.random.normal(shape=[batch_size,input_to_generator])
+
+            gen_images = generator(noisy_image)
+            input_to_discriminator = tf.concat([gen_images,tf.dtypes.cast(x_batch,tf.float32)],axis=0)
+            Labels=tf.constant([[0.0]]*batch_size + [[1.0]]*batch_size)
+            discriminator.trainable = True
+
+            discriminator.train_on_batch(input_to_discriminator,Labels)
+            noisy_image = tf.random.normal(shape=[batch_size,input_to_generator])
+            FakeLabels=tf.constant([[1.0]]*batch_size)
+            discriminator.trainable = False
+
+            GAN.train_on_batch(noisy_image,FakeLabels)
+    noise_images = tf.random.normal(shape=[10,input_to_generator])
+    images = generator(noise_images)
+    for i in images :
+        plt.imshow(i.numpy().reshape(28,28))
+        plt.show()
+
+    model_json = generator.to_json()
+    with open("Trained Model/model"+letters[letter]+".json", "w") as json_file:
+        json_file.write(model_json)
+
+    generator.save_weights("Trained Model/model"+letters[letter]+".h5")
+    print("Saved model to disk")
+
+#Model('A',200,32)
+
+def Generate_Handwritten_Words(WordsPerSentence=6):
+
+    print('Enter a Word/Sentence:')
+    userinput = input()
+    userinput = userinput.upper()
+    spaces = 0
+    spacesindices = []
+    for  index , i in enumerate(list(userinput)) :
+        if i == " ":
+            spaces += 1
+            spacesindices.append(index)
+        if i not in letters:
+            print('Input should contain only characters from A to Z')
+            print('\t ************************************')
+            return
+
+    myword = list(userinput)
+    print(myword)
+
+
+    numoflines = math.ceil(spaces/WordsPerSentence)
+    print(numoflines)
+    if spaces > WordsPerSentence :
+        while spaces < WordsPerSentence * numoflines + 1:
+            spacesindices.append(spacesindices[-1]+WordsPerSentence)
+            spaces +=1
+
+    print(spacesindices)
+    numoflines = math.ceil(spaces/WordsPerSentence)
+    print('Number of lines: ', numoflines)
+    spacesindices2 = []
+
+    for k , n in enumerate(spacesindices) :
+        if k % WordsPerSentence == 0 and k != 0:
+            print('true')
+            spacesindices2.append(n)
+
+    images = []
+    row=[]
+    m = -1
+    if numoflines > 1:
+        for j in range(0 , numoflines-1):
+            m +=1
+            images = []
+            for k , i in enumerate(myword):
+                print(letters[i])
+
+                if k < spacesindices2[m] and (k > spacesindices2[m - 1] or m - 1 == -1):
+
+                    if letters[i] < 26 :
+
+                        json_file = open('Trained Model/model' + str(letters[i]) + '.json', 'r')
+                        loaded_model_json = json_file.read()
+                        json_file.close()
+                        loaded_model = model_from_json(loaded_model_json)
+
+                        loaded_model.load_weights("Trained Model/model" + str(letters[i]) + ".h5")
+                        print("Loaded model from disk")
+
+                        noise = tf.random.normal(shape=[1, 100])
+                        image = loaded_model(noise)
+                        image = image.numpy().reshape(28, 28)
+                        image = cv2.resize(image, (56, 56))
+                        images.append(image)
+
+
+                    else:
+
+                        x = np.empty([56, random.randint(50,60)], dtype=np.float32)
+                        x.fill(-1)
+                        images.append(x)
+
+            print(images)
+            x = np.concatenate([i for i in images], 1)
+            row.append(x)
+
+        max_width = 0
+        for i in row :
+            if i.shape[1] > max_width:
+                max_width = i.shape[1]
+        print(max_width)
+        words=[]
+        for i in row:
+            print('**Prev Shape**',i.shape)
+            if i.shape[1] < 1400 :
+                x = np.empty([56, max_width-i.shape[1]], dtype=np.float32)
+                x.fill(-1)
+                words.append(np.concatenate([i,x],1))
+            else :
+                x = cv2.resize(i, dsize=(max_width, 56),interpolation=cv2.INTER_CUBIC)
+                words.append(x)
+        x = np.concatenate([i for i in words], 0)
+        x = (255 - x) #to invert image colors
+        x=cv2.medianBlur(x, 3) #Median filter to remove the noise
+        x=cv2.GaussianBlur(x,(3,3),1)#Gaussian filter to smooth the image
+        plt.imshow(x,cmap='gray')
+        plt.xticks([])
+        plt.yticks([])
+        plt.show()
+    else:
+        for i in myword:
+            if letters[i] < 26 :
+                json_file = open('Trained Model/model' + str(letters[i]) + '.json', 'r')
+                loaded_model_json = json_file.read()
+                json_file.close()
+                loaded_model = model_from_json(loaded_model_json)
+
+                loaded_model.load_weights("Trained Model/model" + str(letters[i]) + ".h5")
+                print("Loaded model from disk")
+
+                noise = tf.random.normal(shape=[1, 100])
+                image = loaded_model(noise)
+                image = image.numpy().reshape(28, 28)
+                image = cv2.resize(image, (56, 56))
+                images.append(image)
+
+            else:
+
+                x = np.empty([56, random.randint(45,55)], dtype=np.float32)
+                x.fill(-1)
+                images.append(x)
+
+            print(images)
+        x = np.concatenate([i for i in images], 1)
+        x = (255 - x) #to invert image colors
+        x=cv2.medianBlur(x, 3) #Median filter to remove the noise
+        x=cv2.GaussianBlur(x,(3,3),1) #Gaussian filter to smooth the image
+        plt.imshow(x,cmap='gray')
+        plt.xticks([])
+        plt.yticks([])
+        plt.show()
+
+Generate_Handwritten_Words(6)
