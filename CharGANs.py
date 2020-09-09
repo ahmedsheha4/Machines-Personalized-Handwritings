93b7bfc86f1a123afac43e3326713ec1f0736312
Ahmed Sheha <70905318+ahmedsheha4@users.noreply.github.com>
2020-09-09 18:19:40 +0200

    Update CharGANs.py

--- CharGANs.py
+++ CharGANs.py
@@ -10,6 +10,8 @@ from PIL import Image
 import os
 import  random
 
+letters = {"A":0,"B":1,"C":2,"D":3,"E":4,"F":5,"G":6,"H":7,"I":8,"J":9,"K":10,"L":11,"M":12,"N":13,"O":14,"P":
+           15,"Q":16,"R":17,"S":18,"T":19,"U":20,"V":21,"W":22,"X":23,"Y":24,"Z":25," ":26 , ",":27 , ".":28}
 
 def Model(letter,num_of_epochs,batchSize):
     data = []
